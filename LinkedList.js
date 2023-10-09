df85b0da8ca3be471dc692ca98978b9aec6ef6dd
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-08 19:50:33 -0500

    Create functions to prepend/append nodes

--- LinkedList.js
+++ LinkedList.js
@@ -5,16 +5,41 @@ import Node from './Node.js';
  */
 export default class LinkedList {
   /**
+   * Create empty linked list
    */
   constructor() {
     this.head = null;
     this.size = 0;
   }
 
-  // Insert first node
+  /**
+   * Insert first node (head)
+   * @param {*} value data
+   */
+  prepend(value) {
+    this.head = new Node(value, this.head);
+    this.size++;
+  }
+  /**
+   * Insert last node (tail)
+   * @param {*} value data
+   */
+  append(value) {
+    const node = new Node(value);
+    let current;
 
-  // Insert last node
-
+    // If there is no head (empty list) make it head
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = node;
+      this.size++;
+    }
+  }
   // Insert at index
 
   // Get at index
