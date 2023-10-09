bae342228894936d3a9d2cee342306760f8d7688
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-08 19:30:18 -0500

    Start work on LinkedList-class

---
+++ LinkedList.js
@@ -0,0 +1,27 @@
+import Node from './Node.js';
+
+/**
+ * @module LinkedList
+ */
+export default class LinkedList {
+  /**
+   */
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert first node
+
+  // Insert last node
+
+  // Insert at index
+
+  // Get at index
+
+  // Remove at index
+
+  // Clear list
+
+  // Print list data
+}
