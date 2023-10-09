85cc26ed471d348de1c4599ec77419d3018ccdbf
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-08 19:25:52 -0500

    Add export and JSDoc comments

--- Node.js
+++ Node.js
@@ -1,4 +1,12 @@
-class Node {
+/**
+ * @module Node
+ *
+ */
+export default class Node {
+  /**
+   * @param {*} data data for the node
+   * @param {*} next pointer to next node
+   */
   constructor(data, next = null) {
     this.data = data;
     this.next = next;
